a4b7e603110c9f2e0702f5c465042b46c74e9618
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2025-07-16 15:22:14 -0400

    Improve the utils module's integration tests
    Signed-off-by: Jesus Graterol <jesusgraterol.dev@protonmail.com>

--- src/utils/index.test-integration.ts
+++ src/utils/index.test-integration.ts
@@ -1,4 +1,5 @@
 import { describe, test, expect } from 'vitest';
+import { IUUIDVersion } from '../shared/types.js';
 import { ERRORS } from '../shared/errors.js';
 import { isIntegerValid, isUUIDValid } from '../validations/index.js';
 import {
@@ -15,73 +16,46 @@ import {
 
 describe('Generators', () => {
   describe('generateUUID', () => {
-    test('can generate a valid uuid v4', () => {
-      [
-        generateUUID(4),
-        generateUUID(4),
-        generateUUID(4),
-        generateUUID(4),
-        generateUUID(4),
-        generateUUID(4),
-        generateUUID(4),
-        generateUUID(4),
-        generateUUID(4),
-        generateUUID(4),
-        generateUUID(4),
-        generateUUID(4),
-        generateUUID(4),
-        generateUUID(4),
-        generateUUID(4),
-      ].forEach((uuid) => {
-        expect(typeof uuid).toBe('string');
-        expect(uuid.length).toBe(36);
-        expect(isUUIDValid(uuid, 4)).toBe(true);
-        expect(
-          /^[a-zA-Z0-9]{8}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{12}$/.test(
-            uuid,
-          ),
-        ).toBe(true);
-      });
-    });
-
-    test('can generate a valid uuid v7', () => {
-      [
-        generateUUID(7),
-        generateUUID(7),
-        generateUUID(7),
-        generateUUID(7),
-        generateUUID(7),
-        generateUUID(7),
-        generateUUID(7),
-        generateUUID(7),
-        generateUUID(7),
-        generateUUID(7),
-        generateUUID(7),
-        generateUUID(7),
-        generateUUID(7),
-        generateUUID(7),
-        generateUUID(7),
-      ].forEach((uuid) => {
-        expect(typeof uuid).toBe('string');
-        expect(uuid.length).toBe(36);
-        expect(isUUIDValid(uuid, 7)).toBe(true);
-        expect(
-          /^[a-zA-Z0-9]{8}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{12}$/.test(
-            uuid,
-          ),
-        ).toBe(true);
-      });
+    test.each(<Array<[IUUIDVersion]>>[
+      [4],
+      [4],
+      [4],
+      [4],
+      [4],
+      [4],
+      [4],
+      [4],
+      [4],
+      [4],
+      [7],
+      [7],
+      [7],
+      [7],
+      [7],
+      [7],
+      [7],
+      [7],
+      [7],
+      [7],
+    ])('generateUUID(%i) -> valid UUID', (version) => {
+      const uuid = generateUUID(version);
+      expect(typeof uuid).toBe('string');
+      expect(uuid.length).toBe(36);
+      expect(isUUIDValid(uuid, version)).toBe(true);
+      expect(
+        /^[a-zA-Z0-9]{8}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{12}$/.test(uuid),
+      ).toBe(true);
     });
   });
 
   describe('generateRandomString', () => {
-    test('can generate a random string of any length', () => {
-      const lengths = [5, 15, 35, 75, 100];
-      const strings = lengths.map((len) => generateRandomString(len));
-      strings.forEach((str, i) => {
-        expect(new RegExp(`^[A-Za-z0-9]{${lengths[i]}}$`).test(str)).toBe(true);
-      });
-    });
+    test.each(<Array<[number]>>[[5], [15], [35], [75], [100]])(
+      'generateRandomString(%i) -> valid string',
+      (length) => {
+        const str = generateRandomString(length);
+        expect(new RegExp(`^[A-Za-z0-9]{${length}}$`).test(str)).toBe(true);
+      },
+    );
 
     test('can generate a random string out a custom list of characters', () => {
       expect(/^[ABCDEFG]{100}$/.test(generateRandomString(100, 'ABCDEFG'))).toBe(true);
